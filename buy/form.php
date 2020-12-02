1d68fa61fefd1c8ee4f4ac8113f319c1d3a648f3
gawgaw <gawgaw.developer@gmail.com>
2020-12-02 13:15:00 +0900

    会計入力と確認画面を作成

---
+++ buy/form.php
@@ -0,0 +1,76 @@
+<?php include(dirname(__FILE__).'/../assets/_inc/_head.php'); ?>
+<?php include(dirname(__FILE__).'/../assets/_inc/_header.php'); ?>
+
+  <main>
+    <section class="form">
+      <div class="section-inner">
+        <div class="registration-steps">
+          <div class="registration-steps_step -active">
+            <div class="num">1</div>
+            <div class="text">ユーザー情報入力</div>
+          </div>
+          <div class="registration-steps_line"></div>
+          <div class="registration-steps_step">
+            <div class="num">2</div>
+            <div class="text">ご注文内容確認</div>
+          </div>
+          <div class="registration-steps_line"></div>
+          <div class="registration-steps_step">
+            <div class="num">3</div>
+            <div class="text">ユーザー登録完了</div>
+          </div>
+        </div>
+        <form action="/buy/form_check.php" method="post">
+          <p class="form-title label label--price">お客様情報の入力</p>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_name">お名前</label>
+            <input id="js-input-user_name" class="input-box__input" type="text" name="user_name" value="" autofocus >
+          </div>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_email">メールアドレス</label>
+            <input id="js-input-user_email" class="input-box__input" type="email" name="user_email" value="">
+          </div>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_postal">郵便番号</label>
+            <input id="js-input-user_postal" class="postal1 input-box__input" type="text" name="postal1" value="">-
+            <input class="postal2 input-box__input" type="text" name="postal2" value="">
+          </div>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_address">住所</label>
+            <input id="js-input-user_address" class="input-box__input" type="text" name="user_address" value="">
+          </div>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_tel">電話番号</label>
+            <input id="js-input-user_tel" class="input-box__input" type="text" name="user_tel" value="">
+          </div>
+          <div class="radio-box">
+            <p class="radio-box__label">注文区分</p>
+            <div class="radio-box__item">
+              <input class="radio-box__radio" type="radio" id="wifi1" name="wifi" value="有り">
+              <label for="wifi1">今回だけの注文</label>
+            </div>
+            <div class="radio-box__item">
+              <input class="radio-box__radio" type="radio" id="wifi2" name="wifi" value="無し">
+              <label for="wifi2">会員登録して注文</label>
+            </div>
+            <p>会員登録して注文すると、次回からお客様情報の入力を省く事ができます</p>
+          </div>
+          <p class="form-title label label--price">会員登録する方はパスワードを設定してください</p>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_password">パスワード</label>
+            <input class="input-box__input" id="js-input-user_password" type="password" name="user_password" value="" autocomplete="off">
+          </div>
+          <div class="input-box">
+            <label class="input-box__label" for="js-input-user_password2">パスワード【確認用】</label>
+            <input class="input-box__input" id="js-input-user_password2" type="password" name="user_password2" value="" autocomplete="off">
+          </div>
+          <div class="form__btns">
+            <button class="btn btn--ok">入力内容を確認する</button>
+            <a class="btn btn--back" href="/cart/">カートへ戻る</a>
+          </div>
+        </form>
+      </div>
+    </section>
+  </main>
+
+<?php include(dirname(__FILE__).'/../assets/_inc/_footer.php'); ?>
---
+++ buy/form_check.php
@@ -0,0 +1,62 @@
+<?php include(dirname(__FILE__).'/../assets/_inc/_head.php'); ?>
+<?php include(dirname(__FILE__).'/../assets/_inc/_header.php'); ?>
+
+  <main>
+    <section class="form-check">
+      <div class="section-inner">
+        <div class="registration-steps">
+          <div class="registration-steps_step">
+            <div class="num">1</div>
+            <div class="text">ユーザー情報入力</div>
+          </div>
+          <div class="registration-steps_line"></div>
+          <div class="registration-steps_step -active">
+            <div class="num">2</div>
+            <div class="text">ご注文内容確認</div>
+          </div>
+          <div class="registration-steps_line"></div>
+          <div class="registration-steps_step">
+            <div class="num">3</div>
+            <div class="text">ユーザー登録完了</div>
+          </div>
+        </div>
+        <dl class="user-check-list">
+          <dt class="user-check-list__title">お名前</dt>
+          <dd class="user-check-list__data">橋本浩二</dd>
+          <dt class="user-check-list__title">メールアドレス</dt>
+          <dd class="user-check-list__data">apple@apple.jp</dd>
+          <dt class="user-check-list__title">郵便番号</dt>
+          <dd class="user-check-list__data">345-6367</dd>
+          <dt class="user-check-list__title">住所</dt>
+          <dd class="user-check-list__data">千葉県千葉市佐伯区美鈴が丘西2-54-6</dd>
+          <dt class="user-check-list__title">電話番号</dt>
+          <dd class="user-check-list__data">080-6854-5678</dd>
+        </dl>
+        <ul class="cart__list">
+          <li class="cart-item">
+            <p class="cart-item__image"><img src="/assets/image/product-picture/item-sample01@2x.jpg" alt=""></p>
+            <p class="cart-item__title">モッツァレラと厳選チーズのセット</p>
+            <div class="cart-item__kazu">2個</div>
+            <p class="cart-item__price">¥ 3,600</p>
+          </li>
+          <li class="cart-item">
+            <p class="cart-item__image"><img src="/assets/image/product-picture/item-sample04@2x.jpg" alt=""></p>
+            <p class="cart-item__title">モッツァレラと厳選チーズのセット</p>
+            <div class="cart-item__kazu">3個</div>
+            <p class="cart-item__price">¥ 3,600</p>
+          </li>
+        </ul>
+        <div class="cart__footer">
+          <p class="cart__sum">合計 5,500円</p>
+        </div>
+        <form action="" method="post">
+          <div class="form__btns">
+            <button class="btn btn--ok">次へ/支払い情報の入力</button>
+            <a class="btn btn--back" href="/buy/form.php">戻る/入力内容を訂正する</a>
+          </div>
+        </form>
+      </div>
+    </section>
+  </main>
+
+<?php include(dirname(__FILE__).'/../assets/_inc/_footer.php'); ?>
