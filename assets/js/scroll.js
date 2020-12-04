2a8f906d33f54bb8869291762525c0c492336fe6
gawgaw <gawgaw.developer@gmail.com>
2020-12-04 14:25:31 +0900

    animateCSSのクラスの修正

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,9 +3,9 @@ $(function() {
   $('.test').on('inview', function(event, isInView) {
     $('test').each(function() {
       if (isInView) {
-        $(this).addClass('animated rollIn');
+        $(this).addClass('animate__animated animate__rollIn');
       } else {
-        $(this).removeClass('animated rollIn');
+        $(this).removeClass('animate__animated animate__rollIn');
       }
     });
   });
@@ -17,17 +17,17 @@ $(function() {
   // ロゴ
   $('.logo02').on('inview', function(event, isInView) {
     if (isInView) {
-      $('.logo02').addClass('animated rollIn');
+      $('.logo02').addClass('animate__animated animate__rollIn');
     } else {
-      $('.logo02').removeClass('animated rollIn');
+      $('.logo02').removeClass('animate__animated animate__rollIn');
     }
   });
   // ロゴ
   $('.logo').on('inview', function(event, isInView) {
     if (isInView) {
-      $('.logo').addClass('animated rollIn');
+      $('.logo').addClass('animate__animated animate__rollIn');
     } else {
-      $('.logo').removeClass('animated rollIn');
+      $('.logo').removeClass('animate__animated animate__rollIn');
     }
   });
 
@@ -35,72 +35,72 @@ $(function() {
   
   $('#js-introduce__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-part-of-item__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-member__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-strength__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-new__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-products__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-detail__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-strength__heading02').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-detail-more__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
   $('#js-carousel__heading').on('inview', function(event, isInView) {
     if (isInView) {
-      $(this).addClass('animated fadeInUp');
+      $(this).addClass('animate__animated animate__fadeInUp');
     } else {
-      $(this).removeClass('animated fadeInUp');
+      $(this).removeClass('animate__animated animate__fadeInUp');
     }
   });
 
@@ -108,45 +108,45 @@ $(function() {
   // 商品ページの中間イメージ
   $('.js-pulse').on('inview', function(event, isInView) {
     if (isInView) {
-      $('.js-pulse').addClass('animated pulse');
+      $('.js-pulse').addClass('animate__animated animate__pulse');
     } else {
-      $('.js-pulse').removeClass('animated pulse');
+      $('.js-pulse').removeClass('animate__animated animate__pulse');
     }
   });
 
   // トップページの最初の画像その1
   $('#intro-box01').on('inview', function(event, isInView) {
     if (isInView) {
-      $('#intro-box01').addClass('animated fadeIn');
+      $('#intro-box01').addClass('animate__animated animate__fadeIn');
     } else {
-      $('#intro-box01').removeClass('animated fadeIn');
+      $('#intro-box01').removeClass('animate__animated animate__fadeIn');
     }
   });
 
   // トップページの最初の画像その2
   $('#intro-box02').on('inview', function(event, isInView) {
     if (isInView) {
-      $('#intro-box02').addClass('animated fadeIn');
+      $('#intro-box02').addClass('animate__animated animate__fadeIn');
     } else {
-      $('#intro-box02').removeClass('animated fadeIn');
+      $('#intro-box02').removeClass('animate__animated animate__fadeIn');
     }
   });
   
   // メンバープロフィールのテキストその1
   $('#profile-card__text01').on('inview', function(event, isInView) {
     if (isInView) {
-      $('#profile-card__text01').addClass('animated fadeInLeft');
+      $('#profile-card__text01').addClass('animate__animated animate__fadeInLeft');
     } else {
-      $('#profile-card__text01').removeClass('animated fadeInLeft');
+      $('#profile-card__text01').removeClass('animate__animated animate__fadeInLeft');
     }
   });
 
   // メンバープロフィールのテキストその2
   $('#profile-card__text02').on('inview', function(event, isInView) {
     if (isInView) {
-      $('#profile-card__text02').addClass('animated fadeInLeft');
+      $('#profile-card__text02').addClass('animate__animated animate__fadeInLeft');
     } else {
-      $('#profile-card__text02').removeClass('animated fadeInLeft');
+      $('#profile-card__text02').removeClass('animate__animated animate__fadeInLeft');
     }
   });
 
